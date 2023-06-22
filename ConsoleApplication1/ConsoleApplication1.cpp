77aeb65c5f060b76573f1025bcb90762f5b3a4e8
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-22 11:09:57 +0700

    menabahkan fungsi FRONT_position > REAR_position, iterasi dari front hingga akhir array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -78,6 +78,14 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari front hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
 		}
 	}
 };
