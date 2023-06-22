ff3732aecb7696170e0a85fdb7c59c2a51a5f7e3
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-22 11:13:03 +0700

    Iterasi dari awal array hingga Rear

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -86,6 +86,15 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			//Iterasi dari awal Array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
