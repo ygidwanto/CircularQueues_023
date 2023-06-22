98ded7689985604ff2bf7408512ffedbf5a2ce65
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-22 10:58:32 +0700

    element yang sudah dihapus berada di posisis terakhir pada array, kembali ke awal array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -51,5 +51,12 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			// Jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 };
