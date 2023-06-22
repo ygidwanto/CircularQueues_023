3054af4e48ab2efa8ed599ec0c35ebe764ae453e
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-22 11:07:01 +0700

    menambahakan  FRONT_position <=REAR_position, iterasi dari front hingga rear

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -69,5 +69,15 @@ public:
 			cout << " Queue is empty\n";
 			return;
 		}
+
+		cout << "\nElements in the queue are: \n";
+
+		// Jika FRONT_position <=REAR_position, iterasi dari front hingga rear
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+		}
 	}
 };
